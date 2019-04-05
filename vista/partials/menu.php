a4532baa412d969906a2560c4865b05e18fcd5cd
Ttito19 <andrezchavez.2000@gmail.com>
2019-04-05 13:13:07 -0500

    header

--- vista/partials/menu.php
+++ vista/partials/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light" style="background-color: #0C082B;">
+<!--<nav class="navbar navbar-light" style="background-color: #0C082B;">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -10,7 +10,29 @@
           <a class="navbar-brand">Good Partner Consulting</a>
         </div>
       </div>
-    </nav>
+    </nav>-->
+
+    <nav class="navbar navbar-expand-sm  navbar-dark" style="background-color:#0C082B;" >
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">inhabilitado</a>
+          </li>
+        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+        <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>    
+      </nav>
 
 
 
